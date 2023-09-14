214a4c2171af666b3eaa125a0321236e63e8a153
Evelin <evelimcarreira@gmail.com>
2023-09-14 20:45:44 -0300

    Criando componente listar-usuarios

--- src/app/lista-usuario/lista-usuario.component.ts
+++ src/app/lista-usuario/lista-usuario.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from './model/lista-usuario.module';
 
 @Component({
   selector: 'app-lista-usuario',
@@ -8,7 +9,8 @@ import { Component, OnInit } from '@angular/core';
 export class ListaUsuarioComponent implements OnInit {
 
   constructor() { }
-
+  usuarios: Usuario[] = [];
+  
   ngOnInit(): void {
   }
 
---
+++ src/app/lista-usuario/model/lista-usuario.module.ts
@@ -0,0 +1,9 @@
+
+export class Usuario {
+    public codigo!: number;
+    public nome!: string;
+    public cpf!: string;
+    public perfil!: string;
+    public departamento!: string;
+    public situacao!: string;
+}
